766ad018165ba2f75775f1f007731f6da3f0bca0
JaNaOsk <zhannakhamidullina@gmail.com>
2022-11-21 14:29:48 +0500

    initial commit

--- python_practice/rectangle.py
+++ python_practice/rectangle.py
@@ -1,19 +1,26 @@
 import math
+
+
 class Rectangle:
-    def __init__(self,a,b):
-        self.a= a
-        self.b=b
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
     def get_area(self):
-        return self.a*self.b
+        return self.a * self.b
+
 
 class Square:
-    def __init__(self,a):
-        self.a=a
+    def __init__(self, a):
+        self.a = a
+
     def get_area_square(self):
-        return self.a**2
+        return self.a ** 2
+
 
 class Circle:
-    def __init__(self,r):
-        self.r=r
+    def __init__(self, r):
+        self.r = r
+
     def get_area_circle(self):
-        return self.r**2*math.pi
+        return self.r ** 2 * math.pi
--- python_practice/rectangle_2.py
+++ python_practice/rectangle_2.py
@@ -6,21 +6,21 @@ rect_2 = Rectangle(12, 5)
 print(rect_1.get_area())
 print(rect_2.get_area())
 
-square_1= Square(5)
-square_2=Square(10)
+square_1 = Square(5)
+square_2 = Square(10)
 
 print(square_1.get_area_square(),
       square_2.get_area_square())
 
-circle_1=Circle(5)
+circle_1 = Circle(5)
 
 print(circle_1.get_area_circle())
 
-figures = [rect_1, rect_2,square_1, square_2,circle_1]
+figures = [rect_1, rect_2, square_1, square_2, circle_1]
 for figure in figures:
-      if isinstance(figure, Square):
-            print(figure.get_area_square())
-      elif isinstance(figure, Circle):
-            print(figure.get_area_circle())
-      else:
-            print(figure.get_area())
+    if isinstance(figure, Square):
+        print(figure.get_area_square())
+    elif isinstance(figure, Circle):
+        print(figure.get_area_circle())
+    else:
+        print(figure.get_area())
